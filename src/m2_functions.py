306f4ba50b8bb7cd685f468da56c24b6be811d1d
chenl4 <chenl4@rose-hulman.edu>
2018-03-07 09:27:07 -0500

    blah blah

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -3,12 +3,12 @@ Practice DEFINING and CALLING
      FUNCTIONS
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Lilin Chen.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -32,9 +32,11 @@ def main():
     window = rg.TurtleWindow()
 
     turtle1()
+    turtle4()
     turtle3()
     turtle2()
     turtle2()
+    turtle5()
 
     window.close_on_mouse_click()
 
@@ -104,9 +106,41 @@ def turtle3():
     maja.end_fill()
 
 
+def turtle4():
+    """"""
+    jack = rg.SimpleTurtle('turtle')
+    jack.speed = 3
+    jack.pen = rg.Pen('green',15)
+
+    jack.backward(100)
+    jack.left(45)
+    jack.forward(90)
+
+
+def turtle5():
+    """"""
+    henry = rg.SimpleTurtle('circle')
+    henry.speed = 3
+    henry.pen = rg.Pen('red',20)
+    henry.right(45)
+    henry.forward(150)
+    henry.begin_fill()
+    henry.draw_square(100)
+    henry.end_fill()
+
+    lily = rg.SimpleTurtle('triangle')
+    lily.speed = 3
+    lily.pen = rg.Pen('yellow',25)
+    lily.left(135)
+    lily.forward(150)
+    lily.begin_fill()
+    lily.draw_square(100)
+    lily.end_fill()
+
+
 ###############################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   READ the code above.  Be sure you understand:
 #     -- How many functions are defined above?
 #           (Answer: 4)
@@ -138,7 +172,7 @@ def turtle3():
 
 ###############################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Define another function,
 #   immediately below the end of the definition of   turtle3   above.
 #   Name your new function   turtle4.
@@ -168,7 +202,7 @@ def turtle3():
 
 ###############################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Add a line to   main   that CALLS your new function immediately
 #   AFTER  main  calls turtle1.  So:
 #     -- the SimpleTurtle from turtle1 should move,
@@ -184,7 +218,7 @@ def turtle3():
 
 ###############################################################################
 #
-# TODO: 6.
+# DONE: 6.
 #   The previous two TODOs IMPLEMENTED a function (TO-DO 4)
 #   and TESTED that function (TO-DO 5).
 #
@@ -211,7 +245,7 @@ def turtle3():
 
 ###############################################################################
 #
-# TODO: 7.
+# DONE: 7.
 #   COMMIT-and-PUSH your work (after changing this TO-DO to DONE).
 #
 #   As a reminder, here is how you should do so:
@@ -235,3 +269,4 @@ def turtle3():
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
 main()
+
